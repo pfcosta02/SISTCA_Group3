9a70f1fbcb7c9b2419d5fdcba7788556bde7903b
pfcosta <1201576@isep.ipp.pt>
2023-03-22 23:27:37 +0000

    Carro vira

---
+++ Assets/Scripts/Black_viper.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Black_viper : MonoBehaviour
+{
+    /*
+    private Rigidbody2D rb;
+    public float moveSpeed = 100f;
+    private float direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        direction = Input.GetAxisRaw("Horizontal");
+    }
+
+    // Update is called once per frame
+    private void fixedUpdate()
+    {
+        rb.velocity = Vector2.zero;
+        rb.velocity = new Vector2(direction * moveSpeed * Time.fixedDeltaTime, 0);
+    }
+    */
+    public float carSpeed;
+    Vector3 position;
+
+    void Start() {
+        position = transform.position;
+    }
+
+    void Update() {
+        position.x += Input.GetAxis ("Horizontal") * carSpeed * Time.deltaTime;
+        transform.position = position;
+    }
+}
