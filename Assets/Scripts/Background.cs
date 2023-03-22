ebcb864f14691109c91b940c52d93d89dbe9b38c
Rafael <1201674@isep.ipp.pt>
2023-03-22 12:12:59 +0000

    Add background game, and moving background

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    Renderer rend;
+    public float speed = 0.2f;
+    public bool xAxis;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float offset = Time.time * speed;
+
+        if(!xAxis) {
+            rend.material.SetTextureOffset("_MainTex", new Vector2(0, offset));
+        } else {
+            rend.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+        }
+    }
+}
